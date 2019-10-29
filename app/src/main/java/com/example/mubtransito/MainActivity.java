cc6f0e39a699323dfea89e8cab248152f4a6f3e6
Geovanny Santos <geovannysantosdesousa@gmail.com>
2019-10-29 20:32:13 -0300

    "Implementação do projeto"

---
+++ app/src/main/java/com/example/mubtransito/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mubtransito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btLogin = (Button) findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MenuPrincipal.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
